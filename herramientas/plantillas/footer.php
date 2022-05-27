4c0ef27febdc3426ed338e9243d60c6449b4b028
rodrigo govea <rodry.gm.isc@live.com.mx>
2022-05-27 13:20:49 -0500

    actualización de archivos php

---
+++ herramientas/plantillas/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+        <div class="contenedor contenido__footer">
+            <div class="barra">
+                <a href = "inicio.html">
+                    <!-- <img src="IMG/logo.svg" alt="Logo de la empresa"> -->
+                    <h1 class="logo">Bienes<span class="logo__bold">Raices</span></h1>
+                </a>
+
+                <div>
+                    <nav class="navegacion--secundaria">
+                        <a class="navegacion__enlace" href="/nosotros.html">Nosotros</a>
+                        <a class="navegacion__enlace" href="/anuncios.html">Anuncios</a>
+                        <a class="navegacion__enlace" href="/blog.html">Blog</a>
+                        <a class="navegacion__enlace" href="/contacto.html">Contacto</a>
+                    </nav>
+                </div>
+            </div>
+            <p class="footer__copyright centrar-texto">Todos los derechos resevados 2021&copy;RGM</p>
+    </footer>
+    <script src="JS/scripts.js"></script>
+</body>
+</html>
